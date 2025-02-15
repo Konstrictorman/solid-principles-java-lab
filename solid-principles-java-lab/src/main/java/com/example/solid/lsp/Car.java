02e3859eded9ac57e6614d8ecd887cf7d2c6305b
DavidCastro4444 <davidsantiagocastro4444@gmail.com>
2025-02-15 11:30:33 -0500

    lsp

--- solid-principles-java-lab/src/main/java/com/example/solid/lsp/Car.java
+++ solid-principles-java-lab/src/main/java/com/example/solid/lsp/Car.java
@@ -1,16 +1,35 @@
-public class Car {
+interface Drivable {
+    void drive();
+}
+
+interface Refuelable {
+    void refuel();
+}
+
+interface Rechargeable {
+    void recharge();
+}
+
+class Car implements Drivable, Refuelable {
+    @Override
     public void drive() {
         System.out.println("Conduciendo...");
     }
 
+    @Override
     public void refuel() {
         System.out.println("Repostando...");
     }
 }
 
-class ElectricCar extends Car {
+class ElectricCar implements Drivable, Rechargeable {
     @Override
-    public void refuel() {
-        throw new UnsupportedOperationException("Los coches eléctricos no usan combustible.");
+    public void drive() {
+        System.out.println("Conduciendo...");
+    }
+
+    @Override
+    public void recharge() {
+        System.out.println("Cargando batería...");
     }
 }
