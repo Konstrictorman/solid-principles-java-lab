f5c301fd7690bcc502e86866a327bd85931941c4
Milton Andres Gutierrez Lopez <80470554+MiltonGutierrez@users.noreply.github.com>
2025-02-15 17:00:58 +0000

    Fix test

--- solid-principles-java-lab/src/test/java/com/example/solid/AppTest.java
+++ solid-principles-java-lab/src/test/java/com/example/solid/AppTest.java
@@ -1,16 +1,6 @@
 package com.example.solid;
 
 
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasses({
-    InvoiceTest.class,
-    InvoiceCalculatorTest.class,
-    InvoicePrinterTest.class,
-    InvoiceDbSaverTest.class
-})
 public class AppTest {
 
 }
